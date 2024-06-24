9a31a49adfa32ccb99de4b9ac452f6d64ae28339
seblful <alesha.chimba@gmail.com>
2024-06-24 22:38:07 +0300

    [EDIT] print images

--- MLP.c
+++ MLP.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mnist.h"
 
 // ==== CONSTANTS ====
@@ -9,10 +10,10 @@
 #define OUTPUT_SIZE 10
 
 // ==== DATA ====
-const char trainImageFilename[] = "mnist_data/train-images.idx3-ubyte";
-const char trainLabelFilename[] = "mnist_data/train-labels.idx1-ubyte";
-const char testImageFilename[] = "mnist_data/t10k-images.idx3-ubyte";
-const char testLabelFilename[] = "mnist_data/t10k-labels.idx1-ubyte";
+const char trainImageFilename[] = "data/train-images.idx3-ubyte";
+const char trainLabelFilename[] = "data/train-labels.idx1-ubyte";
+const char testImageFilename[] = "data/t10k-images.idx3-ubyte";
+const char testLabelFilename[] = "data/t10k-labels.idx1-ubyte";
 
 const int IMAGE_HEADER_SIZE = 16;
 const int LABEL_HEADER_SIZE = 8;
@@ -31,11 +32,22 @@ int main()
     // DataItem *trainData = (DataItem *)malloc(TRAIN_SIZE * sizeof(DataItem));
     // DataItem *testData = (DataItem *)malloc(TEST_SIZE * sizeof(DataItem));
 
-    uint8_t **images;
+    uint8_t **trainImages, **testImages;
+    uint8_t *trainLabels, *test_labels;
 
     // Load train and test data
 
-    images = read_mnist_images(trainImageFilename);
+    trainImages = read_mnist_images(trainImageFilename);
+    trainLabels = read_mnist_labels(trainLabelFilename);
+
+    // printf("%d ", sizeof(trainImages));
+    // printf("%d ", sizeof(trainImages[0]));
+    // printf("%d\n", sizeof(trainImages[0][0]));
+
+    for (int i = 0; i < 255; i++)
+    {
+        printf("%d", trainImages[0][i]);
+    };
 
     return 0;
 };
