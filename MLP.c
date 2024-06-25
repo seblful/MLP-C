1655700c471a4a6cf2acbc8b25d40ae03eb543a7
seblful <alesha.chimba@gmail.com>
2024-06-25 12:38:12 +0300

    [EDIT] read images and labels and print mnist

--- MLP.c
+++ MLP.c
@@ -15,9 +15,6 @@ const char trainLabelFilename[] = "data/train-labels.idx1-ubyte";
 const char testImageFilename[] = "data/t10k-images.idx3-ubyte";
 const char testLabelFilename[] = "data/t10k-labels.idx1-ubyte";
 
-const int IMAGE_HEADER_SIZE = 16;
-const int LABEL_HEADER_SIZE = 8;
-
 // Structs
 typedef struct
 {
@@ -33,21 +30,17 @@ int main()
     // DataItem *testData = (DataItem *)malloc(TEST_SIZE * sizeof(DataItem));
 
     uint8_t **trainImages, **testImages;
-    uint8_t *trainLabels, *test_labels;
+    uint8_t *trainLabels, *testLabels;
 
     // Load train and test data
-
     trainImages = read_mnist_images(trainImageFilename);
     trainLabels = read_mnist_labels(trainLabelFilename);
 
-    // printf("%d ", sizeof(trainImages));
-    // printf("%d ", sizeof(trainImages[0]));
-    // printf("%d\n", sizeof(trainImages[0][0]));
+    testImages = read_mnist_images(testImageFilename);
+    testLabels = read_mnist_labels(testLabelFilename);
 
-    for (int i = 0; i < 255; i++)
-    {
-        printf("%d", trainImages[0][i]);
-    };
+    // Print MNIST image by index
+    printMnist(trainImages, trainLabels, 0);
 
     return 0;
 };
